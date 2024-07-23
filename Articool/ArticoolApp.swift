edf9099bb589115390e819df900794c76b3bcd33
adevaykin <adevaykin@users.noreply.github.com>
2024-07-23 14:00:44 -0400

    Initial Commit

---
+++ Articool/ArticoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticoolApp.swift
+//  Articool
+//
+//  Created by Alexander Devaikin on 23.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ArticoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Articool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Articool
+//
+//  Created by Alexander Devaikin on 23.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
